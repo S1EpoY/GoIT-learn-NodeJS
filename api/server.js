1560a21bfaa8563d5381f4e5de165d059fb30e11
Baryshnikov S.S <56022770+BaryshnikovSS@users.noreply.github.com>
2020-04-18 13:56:06 +0300

    Added MONGODB_URL for testing my code with my db.

--- api/server.js
+++ api/server.js
@@ -18,10 +18,11 @@ app.use(cors());
 app.use('/api/contacts', apiContactsRoutes);
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = "mongodb+srv://admin:QHEmWVrvOkCkXMh5@cluster0-ok9sn.mongodb.net/db-contacts" || process.env.MONGODB_URL
 
 async function startServer() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, { 
+        await mongoose.connect(MONGODB_URL, { 
             useUnifiedTopology: true, 
             useNewUrlParser: true, 
             useFindAndModify: false 
